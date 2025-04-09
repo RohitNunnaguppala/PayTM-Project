738aa5b8fa1a7c31fadb88821c0fab0894d55575
RohitNunnaguppala <151500766+RohitNunnaguppala@users.noreply.github.com>
2025-04-09 21:42:02 +0530

    Update Users.jsx

--- Frontend/src/components/Users.jsx
+++ Frontend/src/components/Users.jsx
@@ -11,7 +11,7 @@ export const Users = () => {
     useEffect(() => {
         const fetchUsers = async () => {
             try {
-                const response = await axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + filter, {
+                const response = await axios.get("https://paytm-backend-rmep.onrender.com/api/v1/user/bulk?filter=" + filter, {
                     headers: {
                         Authorization: "Bearer " + localStorage.getItem("token")
                     }
